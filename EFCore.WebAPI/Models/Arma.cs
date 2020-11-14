f12e99671561de5bbbee710ff8fd4b6b1f12cbc0
Lucas Guasselli <Lucaszack265@gmail.com>
2020-11-14 15:20:34 -0300

    Revert "Teste"
    This reverts commit db1b27a585724dcb5ac06affb5fa36d7d871ceed.

--- EFCore.WebAPI/Models/Arma.cs
+++ EFCore.WebAPI/Models/Arma.cs
@@ -8,6 +8,7 @@ namespace EFCore.WebAPI.Models
     public class Arma
     {
         public int Id { get; set; }
+        public string Nome { get; set; }
         public Heroi Heroi { get; set; }
         public int HeroiId { get; set; }
     }
